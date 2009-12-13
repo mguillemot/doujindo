faa9a72c8ddbb736bf361a477c88e3e22da9dc88
erhune <erhune@1bdb3d1c-df69-384c-996e-f7b9c3edbfcf>
2009-12-13 10:25:18 +0000

    [toushou-shop] - Ajustements visuels sur la vue par catégorie
    git-svn-id: svn://npng.org/erhune/touhou-shop/trunk@114 1bdb3d1c-df69-384c-996e-f7b9c3edbfcf

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -17,24 +17,28 @@ class CategoryController < ApplicationController
       @sort = params[:sort]
       @page = Integer(params[:page])
       items = admin? ? @category.all_items : @category.visible_items
-      @npages = (items.length.to_f / ITEMS_PER_PAGE).ceil
-      if @page < 1
-        @page = 1
-      elsif @page > @npages
-        @page = @npages
+      if items.empty?
+        @display_items = [ ]
+      else
+        @npages = (items.length.to_f / ITEMS_PER_PAGE).ceil
+        if @page < 1
+          @page = 1
+        elsif @page > @npages
+          @page = @npages
+        end
+        items.sort! { |a, b| b.created_at <=> a.created_at }
+        case @sort
+          when 'by-name'
+            items.sort! { |a, b| a.title <=> b.title }
+          when 'by-price'
+            items.sort! { |a, b| a.price <=> b.price }
+          when 'by-type'
+            items.sort! { |a, b| a.item_type <=> b.item_type }
+          when 'by-availability'
+            items.sort! { |a, b| b.availability_type <=> a.availability_type }
+        end
+        @display_items = items[((@page-1)*ITEMS_PER_PAGE)..(@page*ITEMS_PER_PAGE-1)]
       end
-      items.sort! { |a, b| b.created_at <=> a.created_at }
-      case @sort
-        when 'by-name'
-          items.sort! { |a, b| a.title <=> b.title }
-        when 'by-price'
-          items.sort! { |a, b| a.price <=> b.price }
-        when 'by-type'
-          items.sort! { |a, b| a.item_type <=> b.item_type }
-        when 'by-availability'
-          items.sort! { |a, b| b.availability_type <=> a.availability_type }
-      end
-      @display_items = items[((@page-1)*ITEMS_PER_PAGE)..(@page*ITEMS_PER_PAGE-1)]
       logger.debug "Display category #{@category.id} sorting #{@sort} starting at page #{@page}"
     end
   end
